1eba0d07c07be714c28e04a4065174e9e6486448
C. Mitch Carroll <c.mitch.carroll@gmail.com>
2012-05-11 15:57:59 -0700

    added GLUT support, and (main-loop)

--- src/main.c
+++ src/main.c
@@ -5,6 +5,15 @@
 #ifdef HAVE_LIBGUILE
 #include <guile/gh.h>
 #endif
+#ifdef HAVE_LIBGL
+#include <GL/gl.h>
+#endif
+#ifdef HAVE_LIBGLU
+#include <GL/glu.h>
+#endif
+#ifdef HAVE_LIBGLUT
+#include <GL/glut.h>
+#endif
 
 SCM helloWorld()
 {
@@ -15,12 +24,17 @@ SCM helloWorld()
 void init(int argc, char **argv)
 {
   gh_new_procedure("hello-world", helloWorld, 0,0,0);
+  gh_new_procedure("main-loop", glutMainLoop, 0,0,0);
   gh_repl(argc,argv);
 }
 
 int main(int argc, char **argv)
 {
-  
+  glutInit(&argc,argv);
+  glutInitWindowSize(512,512);
+  glutInitWindowPosition(10,10);
+  glutCreateWindow(PACKAGE_STRING);
+  glutInitDisplayMode(GLUT_RGBA|GLUT_DOUBLE|GLUT_DEPTH);
   gh_enter(argc,argv,init);
   return 0;
 }
